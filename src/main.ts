5c67d045a1687be2eae08cc29e1fca847bbfb047
taka01150810 <taka01150810@gmail.com>
2022-11-20 14:03:40 +0900

    expressのセットアップ(エラーハンドリングの設定)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import 'express-async-errors'
 import morgan from 'morgan'
 
 const PORT = 3000
@@ -13,6 +14,24 @@ app.get('/api/hello', async (req, res) => {
   })
 })
 
+app.get('/api/error', async (req, res) => {
+    throw new Error('Error endpoint')
+})
+
+app.use(errorHandler)
+
 app.listen(PORT, () => {
   console.log(`リバーシのアプリケーションがスタートしました: http://localhost:${PORT}`)
 })
+
+function errorHandler(
+    err: any,
+    _req: express.Request,
+    res: express.Response,
+    _next: express.NextFunction
+  ) {
+    console.error('Unexpected error occurred', err)
+    res.status(500).send({
+      message: '予期しないエラーが起きました'
+    })
+}
