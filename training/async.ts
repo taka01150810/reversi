9434f3d1665b53de302764b683aadd3f651531d6
taka01150810 <taka01150810@gmail.com>
2022-11-20 12:09:49 +0900

    関数・コールバックと async/await を用いた非同期処理(async/await を用いた非同期処理)

--- training/async.ts
+++ training/async.ts
@@ -4,42 +4,12 @@ import util from 'util'
 // プロミスというものを使う新しいpromisifyReadFileという変数を作成
 const promisifyReadFile = util.promisify(fs.readFile)
 
-function main() {
-  let fileContent: string = 'Not loaded'
-
-  //非同期処理 処理が進んでいるときにその処理を止めずに別途処理を行うこと
-  //ファイルの読み込みが終わったら実行する readFile
-  fs.readFile('package.json', (err, data) => {
-    // promisifyReadFileはファイルを読み込むという約束をしている
-    const readFilePromise = promisifyReadFile('package.json')
-
-    // 約束が果たされたら処理を行う
-    readFilePromise.then((data) => {
-      const fileContent = data.toString()
-      console.log(fileContent)
-      /* 結果
-      {
-        "name": "tp1",
-        "version": "1.0.0",
-        "description": "",
-        "main": "index.js",
-        "scripts": {
-          "test": "echo \"Error: no test specified\" && exit 1"
-        },
-        "author": "",
-        "license": "ISC",
-        "devDependencies": {
-          "@types/node": "^18.11.9",
-          "ts-node": "^10.9.1",
-          "typescript": "^4.8.3"
-        }
-      }
-      */
-    })
-  })
-
+// awaitとasyncはセット
+async function main() {
+  // 約束を果たすのを待ってくれる await
+  const data = await promisifyReadFile('package.json')
+  const fileContent = data.toString()
   console.log(fileContent)
-  //結果 Not loaded
 }
 
 main()
